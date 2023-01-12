e688c21a8af732027d28e1f24e58b465ac80e13a
abhaymn <abhaykrishnatsy007>
2023-01-12 17:51:51 +0530

    post request

--- src/App.js
+++ src/App.js
@@ -15,22 +15,25 @@ function App() {
     setIsLoading(true);
     setError(null);
     try{
-      const response=await fetch('https://swapi.dev/api/films');
+      const response=await fetch('https://react-http-da031-default-rtdb.firebaseio.com/movies.json');
       if(!response.ok){
         throw new Error('Something went wrong ...Retrying');
       }
 
       const data=await response.json();
+      
+      const loadedMovies=[];
+
+      for(const key in data){
+        loadedMovies.push({
+          id:key,
+          title:data[key].title,
+          openingText:data[key].openingText,
+          releaseDate:data[key].releaseDate,
+        });
+      }
     
-      const tranformedMovies=data.results.map((movieData)=>{
-        return {
-          id:movieData.episode_id,
-          title:movieData.title,
-          openingText:movieData.opening_crawl,
-          releaseDate:movieData.release_date
-        }
-      });
-      setMovies(tranformedMovies);
+      setMovies(loadedMovies);
       setIsLoading(false);
     }
     catch (error){
@@ -42,6 +45,19 @@ function App() {
   useEffect(()=>{
     MovieHandler();
   }, [MovieHandler]);
+
+  async function addMovieHandler(movie){
+    const response=await fetch('https://react-http-da031-default-rtdb.firebaseio.com/movies.json',{
+      method: 'POST',
+      body:JSON.stringify(movie),
+      headers:{
+        'Content-Type':'application/json'
+      }
+    });
+    const data=await response.json();
+    console.log(data);
+      
+  }
 
   let content=<p>Found no movies</p>
 
@@ -60,12 +76,15 @@ function App() {
   return (
     <React.Fragment>
       <section>
-      <Form/>
+      <Form onAddMovie={addMovieHandler}/>
+      
       </section>
       <section>
+      
         <button onClick={MovieHandler} >Fetch Movies</button>
       </section>
       <section>
+      
         {content}
       </section>
     </React.Fragment>
